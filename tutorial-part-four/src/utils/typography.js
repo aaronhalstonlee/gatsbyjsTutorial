41e9ebbef0e0ffbe7813df0fcb116c3c2b484d69
aaronhalstonlee <aaronhalstonlee@gmail.com>
2018-02-10 10:03:32 -0800

    change theme

--- tutorial-part-four/src/utils/typography.js
+++ tutorial-part-four/src/utils/typography.js
@@ -1,6 +1,6 @@
 import Typography from "typography";
-import kirkhamTheme from "typography-theme-kirkham";
+import githubTheme from "typography-theme-github";
 
-const typography = new Typography(kirkhamTheme);
+const typography = new Typography(githubTheme);
 
 export default typography;
